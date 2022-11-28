ae1e7037074d65eccff3d14771c4f3393c401fe3
Katherine Griffin <42548585+kag728@users.noreply.github.com>
2022-11-28 14:38:29 -0500

    Update test.tsx

--- test.tsx
+++ test.tsx
@@ -2,6 +2,5 @@ ${kafkaConfiguration.kafka.prefix:-devdarth}-multiEditAddEditTopic.Ready
 
 ${kafkaConfiguration.kafka.prefix:-devdarth}-myNameIsKatherine.Ready
 
-multiEditAddEditTopic.Ready
 ${kafkaConfiguration.kafka.prefix:-devdarth}- multiEditAddEditTopic.Ready
 ${kafkaConfiguration.kafka.prefix:-devdarth} -multiEditAddEditTopic.Ready
